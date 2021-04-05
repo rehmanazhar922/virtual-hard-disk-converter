e37d03f7bc7902d71d28e5a7da427b61fcea784d
rehmanazhar922 <71656350+rehmanazhar922@users.noreply.github.com>
2021-04-05 08:52:51 -0400

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+# channel https://www.youtube.com/channel/UCeLj2WCady6CAvOXxn6xwiQ
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
@@ -15,3 +15,9 @@ echo -n "type source path : "
 read source
 
 VBoxManage clonehd $source $output --format $outputf
+
+notify-send "converter" "task completed! thanks to cyber security922"
+sleep 2
+notify-send "subscribe me" "thanks to cyber security922"
+
+echo "https://www.youtube.com/channel/UCeLj2WCady6CAvOXxn6xwiQ"
